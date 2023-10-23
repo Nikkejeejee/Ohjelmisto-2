77886eb4de7689a6d8d234907ded19be71618fc1
Nichakon Itthikun <nichakon.itthikun@metropolia.fi>
2023-10-23 17:46:21 +0300

    update

---
+++ mod-9/example.py
@@ -0,0 +1,48 @@
+import random
+from tabulate import tabulate
+
+
+class auto():
+    def __init__(self, rekisteri, hnopeus, thnopeus, kmatka):
+        self.rekisteri = rekisteri
+        self.hnopeus = hnopeus
+        self.thnopeus = thnopeus
+        self.kmatka = kmatka
+
+    def kiihdyta(self, muutos):
+        self.thnopeus += muutos
+        if self.thnopeus > self.hnopeus:
+            self.thnopeus = self.hnopeus
+        if self.thnopeus < 0:
+            self.thnopeus = 0
+
+    def kulje(self, tuntimaara):
+        self.kmatka += (tuntimaara * self.thnopeus)
+
+
+autotalli = []
+
+for kierros in range(10):
+    auton_muodostus = auto(f'ABC-{kierros + 1}', random.randint(100, 200), 0, 0)
+    autotalli.append(auton_muodostus)
+
+kierros = 0
+
+while True:
+    kierros += 1
+    if auton_muodostus.kmatka >= 10000:
+        break
+    for auton_muodostus in autotalli:
+        auton_muodostus.kiihdyta(random.randint(-10, 15))
+        auton_muodostus.kulje(1)
+
+# kisojen_maara
+
+yhteenveto = []
+for auton_muodostus in autotalli:
+    yhteenveto.append(
+        (auton_muodostus.rekisteri, auton_muodostus.hnopeus, auton_muodostus.thnopeus, auton_muodostus.kmatka, kierros))
+
+print(tabulate(yhteenveto,
+               headers=['Rekisteri', 'Huippunopeus', 'Tämänhetkinen nopeus', 'Kuljettumatka', 'Kisojen määrä'],
+               tablefmt='double_grid'))
--- mod-9/teht.4.py
+++ mod-9/teht.4.py
@@ -1,17 +1,18 @@
+import random
+from colorama import Fore, Style
+
 class Car:
-    def __init__(self, name, reg_number, max_speed, cur_speed, trav_dist):
-        self.name = name
+    def __init__(self, reg_number, max_speed, cur_speed, trav_dist):
         self.reg_number = reg_number
         self.max_speed = max_speed
         self.cur_speed = cur_speed
         self.trav_dist = trav_dist
 
     def intro(self):
-        print(f'Name: {self.name}\n'
-              f'Registration number: {self.reg_number}\n'
-              f'Maximum speed: {self.max_speed} km/h\n'
-              f'Current speed: {self.cur_speed} km/h\n'
-              f'Traveled distance: {self.trav_dist} km')
+        print(f'Registration number: {Fore.CYAN}{self.reg_number}{Style.RESET_ALL}\n'
+              f'Maximum speed: {Fore.GREEN}{self.max_speed} km/h{Style.RESET_ALL}\n'
+              f'Current speed: {Fore.YELLOW}{self.cur_speed} km/h{Style.RESET_ALL}\n'
+              f'Traveled distance: {Fore.MAGENTA}{self.trav_dist} km{Style.RESET_ALL}')
 
     def accelerate(self, change):
         self.cur_speed += change
@@ -21,18 +22,34 @@ class Car:
             self.cur_speed = 0
 
     def new_speed(self):
-        print(f"{self.name}'s current speed is {self.cur_speed} km/h")
+        print(f"The current speed is {Fore.YELLOW}{self.cur_speed} km/h{Style.RESET_ALL}")
 
     def drive(self, hours):
         change = hours * self.cur_speed
         self.trav_dist += change
 
-
-car = Car('Porsche', 'ABC-123', 142, 0, 0)
-car.intro()
-print("---------")
-
+# Create a list of cars
 cars = []
-for i in range(1, 11):
-    race_car = Car()
+for i in range(10):
+    race_car = Car("ABC-" + str(i + 1), random.randint(100, 200), 0, 0)
+    cars.append(race_car)
 
+rounds = 0
+while True:
+    rounds += 1
+    if race_car.trav_dist >= 10000:
+        break
+    for race_car in cars:
+        race_car.accelerate(random.randint(-10, 15))
+        race_car.drive(1)
+
+leaderboard = [(car.reg_number, car.max_speed, car.cur_speed, car.trav_dist) for car in cars]
+leaderboard.sort(key=lambda x: x[3], reverse=True)
+
+print("Race Leaderboard:")
+for i, (reg_number, max_speed, cur_speed, trav_dist) in enumerate(leaderboard, start=1):
+    print(f"Position {Fore.RED}{i}{Style.RESET_ALL}:"
+          f" Registration number: {Fore.CYAN}{reg_number}{Style.RESET_ALL},"
+          f" Max speed: {Fore.GREEN}{max_speed} km/h{Style.RESET_ALL},"
+          f" Current speed: {Fore.YELLOW}{cur_speed} km/h{Style.RESET_ALL},"
+          f" Traveled distance: {Fore.MAGENTA}{trav_dist} km{Style.RESET_ALL}")
