acf7ff77d1b815f9ac05827a9e6660e532f2ffa8
Nichakon Itthikun <nichakon.itthikun@metropolia.fi>
2023-10-24 15:23:45 +0300

    update

--- mod-9/teht.4.py
+++ mod-9/teht.4.py
@@ -1,6 +1,7 @@
 import random
 from colorama import Fore, Style
 
+
 class Car:
     def __init__(self, reg_number, max_speed, cur_speed, trav_dist):
         self.reg_number = reg_number
@@ -9,10 +10,10 @@ class Car:
         self.trav_dist = trav_dist
 
     def intro(self):
-        print(f'Registration number: {Fore.CYAN}{self.reg_number}{Style.RESET_ALL}\n'
-              f'Maximum speed: {Fore.GREEN}{self.max_speed} km/h{Style.RESET_ALL}\n'
-              f'Current speed: {Fore.YELLOW}{self.cur_speed} km/h{Style.RESET_ALL}\n'
-              f'Traveled distance: {Fore.MAGENTA}{self.trav_dist} km{Style.RESET_ALL}')
+        print(f'Registration number: {self.reg_number}\n'
+              f'Maximum speed: {self.max_speed} km/h\n'
+              f'Current speed: {self.cur_speed} km/h\n'
+              f'Traveled distance: {self.trav_dist} km')
 
     def accelerate(self, change):
         self.cur_speed += change
@@ -22,7 +23,7 @@ class Car:
             self.cur_speed = 0
 
     def new_speed(self):
-        print(f"The current speed is {Fore.YELLOW}{self.cur_speed} km/h{Style.RESET_ALL}")
+        print(f"Current speed is {self.cur_speed} km/h")
 
     def drive(self, hours):
         change = hours * self.cur_speed
@@ -34,14 +35,16 @@ for i in range(10):
     race_car = Car("ABC-" + str(i + 1), random.randint(100, 200), 0, 0)
     cars.append(race_car)
 
-rounds = 0
-while True:
-    rounds += 1
-    if race_car.trav_dist >= 10000:
-        break
+race_finished = True
+
+while race_finished:
     for race_car in cars:
         race_car.accelerate(random.randint(-10, 15))
         race_car.drive(1)
+
+        if race_car.trav_dist >= 10000:
+            race_finished = False
+            break
 
 leaderboard = [(car.reg_number, car.max_speed, car.cur_speed, car.trav_dist) for car in cars]
 leaderboard.sort(key=lambda x: x[3], reverse=True)
