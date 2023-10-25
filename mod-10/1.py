dd887ffd210657fef19e5e96f90400637fd837c2
Nichakon Itthikun <nichakon.itthikun@metropolia.fi>
2023-10-25 15:20:45 +0300

    update

--- mod-10/1.py
+++ mod-10/1.py
@@ -4,11 +4,14 @@ class Elevator:
         self.top_floor = top_floor
         self.current_floor = current_floor
 
-    def go_to_floor(self, move):
+    def go_to_floor(self):
+        move = int(input("Press a floor button:  "))
         if move == self.current_floor:
+            print("\nPlease press a floor button. You are at the same floor.")
             return
         if move < self.bottom_floor or move > self.top_floor:
             return
+
         if move > self.current_floor:
             while move > self.current_floor:
                 self.floor_up()
@@ -19,14 +22,13 @@ class Elevator:
     def floor_up(self):
         if self.current_floor < self.top_floor:
             self.current_floor += 1
-            print(f"Going up. You've arrived at floor {self.current_floor}")
+            print(f"Going up. Floor {self.current_floor}\n")
 
     def floor_down(self):
         if self.current_floor > self.bottom_floor:
             self.current_floor -= 1
-            print(f"Going down. You've arrived at floor {self.current_floor}")
+            print(f"\nGoing down. Floor {self.current_floor}")
 
 
-h = Elevator(0, 5, 0)
-h.go_to_floor(5)
-h.go_to_floor(0)
+h = Elevator(0, 10, 0)
+h.go_to_floor()
