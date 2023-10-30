ebf4fd135b871a3dfa4b4096b5a3e01b67a45b02
Nichakon Itthikun <nichakon.itthikun@metropolia.fi>
2023-10-30 10:49:37 +0200

    update

---
+++ mod-10/2.py
@@ -0,0 +1,10 @@
+
+class Building:
+    def __init__(self, num_bottom, num_top):
+        self.num_bottom = num_bottom
+        self.num_top = num_top
+        self.elevators = 3
+
+
+    def run_elevator(self):
+        pass
