59e511dae06d56ced367ca1dbcc9afb62cd1f526
Nichakon Itthikun <nichakon.itthikun@metropolia.fi>
2023-10-30 18:43:19 +0200

    update

--- mod-10/4.py
+++ mod-10/4.py
@@ -1,52 +1,77 @@
 import random
-
-class Car:
-    def __init__(self, name, speed):
-        self.name = name
-        self.speed = speed
-        self.distance = 0
-
-    def drive(self, hours):
-        self.distance += self.speed * hours
-
-    def __str__(self):
-        return f"{self.name}: {self.distance:.2f} km"
+from tabulate import tabulate
 
 class Race:
-    def __init__(self, name, distance, car_list):
+    def __init__(self, name, distance, cars):
+        """Initialize a race with a name, distance, and a list of cars."""
         self.name = name
-        self.distance = distance  # in kilometres
-        self.car_list = car_list
+        self.distance = distance
+        self.cars = cars
 
     def hour_passes(self):
-        for car in self.car_list:
-            car.drive(1)
-            car.speed += random.uniform(-5, 5)
+        """Simulate one hour of the race, updating car speeds and distances."""
+        for car in self.cars:
+            car.random_speed_change()
+            car.drive()
 
     def print_status(self):
-        print(f"Race: {self.name}")
-        print(f"Distance: {self.distance} km.")
-        for car in self.car_list:
-            print(car)
-        print()
+        """Print the status of the race including car distances and speeds."""
+        print("\nRace Status:")
+        print(f"{'Car':<15}{'Distance (km)':<15}{'Speed (km/h)':<15}")
+        for car in self.cars:
+            print(f"{car.name:<15}{car.distance:.2f:<15}{car.speed:.2f:<15}")
+
+    def leaderboard(self):
+        """Print the race leaderboard sorted by distance traveled."""
+        winning_cars = sorted(self.cars, key=lambda car: car.distance, reverse=True)
+
+        leaderboard_data = []
+        for i, car in enumerate(winning_cars, start=1):
+            leaderboard_data.append([i, car.name, car.distance, car.speed])
+
+        print("\nRACE LEADERBOARD:")
+        table_headers = ["Position", "Car", "Distance (km)", "Speed (km/h)"]
+        table = tabulate(leaderboard_data, headers=table_headers, tablefmt="pretty", numalign="right")
+        print(table)
 
     def race_finished(self):
-        for car in self.car_list:
+        """Check if the race has finished for all cars."""
+        for car in self.cars:
             if car.distance >= self.distance:
                 return True
         return False
 
-cars = [Car(f"Car {i}", random.uniform(150, 200)) for i in range(1, 11)]
+class Car:
+    def __init__(self, name):
+        """Initialize a car with a name, starting at 0 distance and a random speed between 60 and 120 km/h."""
+        self.name = name
+        self.distance = 0
+        self.speed = random.uniform(60, 120)
 
-grand_derby = Race("Grand Demolition Derby", 8000, cars)
+    def random_speed_change(self):
+        """Introduce random speed changes for the car."""
+        self.speed += random.uniform(-5, 5)
 
-hours = 0
-while not grand_derby.race_finished():
-    if hours % 10 == 0:
-        grand_derby.print_status()
-    grand_derby.hour_passes()
-    hours += 1
+    def drive(self):
+        """Simulate the car's movement in one hour."""
+        self.distance += self.speed
 
-# Print the final status of the race
-grand_derby.print_status()
-print("The race has finished!")
+def main():
+    car_names = ["Tesla", "Ferrari", "BMW", "Audi", "Mercedes",
+                 "Lamborghini", "Porsche", "Jaguar", "Maserati", "Lexus"]
+
+    cars = [Car(name) for name in car_names]
+    race = Race("Grand Demolition Derby", 8000, cars)
+
+    hours = 0
+    while not race.race_finished():
+        race.hour_passes()
+        if hours % 10 == 0:
+            race.print_status()
+        hours += 1
+
+    print("\nRACE FINISHED!")
+    race.leaderboard()
+
+if __name__ == "__main__":
+    main()
