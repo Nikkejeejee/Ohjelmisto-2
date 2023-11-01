2977f06607c7c9eae3800740081b6a62037557c8
Nichakon Itthikun <nichakon.itthikun@metropolia.fi>
2023-11-01 13:05:44 +0200

    update

--- mod-12/2-Weather.py
+++ mod-12/2-Weather.py
@@ -1,33 +1,25 @@
 import requests
 
-def kelvin_to_celsius(kelvin_temp):
-    celsius_temp = kelvin_temp - 273.15
-    return round(celsius_temp, 2)
+def kelvin_to_celcius(kelvin):
+    return kelvin -273.15
 
-def get_weather_data(api_key, city):
-    base_url = "https://api.openweathermap.org/data/2.5/weather"
-    params = {
-        'q': city,
+user_input = str(input("Anna paikkakunnan nimi:  "))
+api_key = "9f6ee7702beb29c882ac24b1a06f016f"
+
+base_url = 'https://api.openweathermap.org/data/2.5/weather'
+params = {
+        'q': user_input,
         'appid': api_key
     }
 
-    try:
-        response = requests.get(base_url, params=params)
-        data = response.json()
+res = requests.get(base_url, params=params)
+weather = res.json()
 
-        if response.status_code == 200:
-            weather_description = data['weather'][0]['description']
-            temperature_kelvin = data['main']['temp']
-            temperature_celsius = kelvin_to_celsius(temperature_kelvin)
-            return f"Sää {city}: {weather_description}, lämpötila: {temperature_celsius} °C"
-        else:
-            return "Tietoja ei löytynyt."
+weather_status = weather['weather'][0]['description']
+weather_temp = weather['main']['temp']
 
-    except requests.exceptions.RequestException as e:
-        return f"Virhe: {e}"
-
-if __name__ == '__main__':
-    api_key = "TÄHÄN_API-AVAIN"  # Korvaa tämä oma API-avaimellasi
-    city = input("Anna paikkakunnan nimi: ")
-    weather_data = get_weather_data(api_key, city)
-    print(weather_data)
+print(f'''
+Kaupunki {user_input}
+Säätila: {weather_status}
+Lämpötila: {kelvin_to_celcius(weather_temp):.1f} celcius
+''' )
