e36e0b22b67b5d3808fc5d38f404a29e5366d0d7
Nichakon Itthikun <nichakon.itthikun@metropolia.fi>
2023-11-01 16:23:15 +0200

    update

--- mod-12/1-Chuck_Norris.py
+++ mod-12/1-Chuck_Norris.py
@@ -2,11 +2,5 @@ import requests
 
 url = "https://api.chucknorris.io/jokes/random/"
 
-try:
-    res = requests.get(url).json()
-    joke = res['value']
-    print(joke)
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {e}")
-except ValueError as e:
-    print(f"Error parsing the JSON response: {e}")
+res = requests.get(url).json()
+print(res['value'])
