5c904d72f2cd33440eba263b3cb1624532f8b1b0
Nichakon Itthikun <nichakon.itthikun@metropolia.fi>
2023-11-01 12:22:28 +0200

    update

--- mod-12/2-Weather.py
+++ mod-12/2-Weather.py
@@ -1,38 +1,33 @@
 import requests
 
-api_key = 'YOUR_API_KEY'
+def kelvin_to_celsius(kelvin_temp):
+    celsius_temp = kelvin_temp - 273.15
+    return round(celsius_temp, 2)
 
-def get_weather_info(city_name):
+def get_weather_data(api_key, city):
     base_url = "https://api.openweathermap.org/data/2.5/weather"
     params = {
-        'q': city_name,
-        'appid': api_key,
-        'units': 'metric'  # This will return temperature in Celsius
+        'q': city,
+        'appid': api_key
     }
 
-    response = requests.get(base_url, params=params)
-
-    if response.status_code == 200:
+    try:
+        response = requests.get(base_url, params=params)
         data = response.json()
-        weather_description = data['weather'][0]['description']
-        temperature_celsius = data['main']['temp']
-        return weather_description, temperature_celsius
-    else:
-        print("Error fetching data. Make sure you have entered a valid city name.")
-        return None
 
-def kelvin_to_celsius(kelvin):
-    return kelvin - 273.15
+        if response.status_code == 200:
+            weather_description = data['weather'][0]['description']
+            temperature_kelvin = data['main']['temp']
+            temperature_celsius = kelvin_to_celsius(temperature_kelvin)
+            return f"Sää {city}: {weather_description}, lämpötila: {temperature_celsius} °C"
+        else:
+            return "Tietoja ei löytynyt."
 
-def main():
-    city_name = input("Enter the name of a municipality: ")
-    weather_info = get_weather_info(city_name)
+    except requests.exceptions.RequestException as e:
+        return f"Virhe: {e}"
 
-    if weather_info:
-        description, temperature_kelvin = weather_info
-        temperature_celsius = kelvin_to_celsius(temperature_kelvin)
-        print(f"Weather in {city_name}: {description}")
-        print(f"Temperature: {temperature_celsius:.2f}°C")
-
-if __name__ == "__main":
-    main()
+if __name__ == '__main__':
+    api_key = "TÄHÄN_API-AVAIN"  # Korvaa tämä oma API-avaimellasi
+    city = input("Anna paikkakunnan nimi: ")
+    weather_data = get_weather_data(api_key, city)
+    print(weather_data)
