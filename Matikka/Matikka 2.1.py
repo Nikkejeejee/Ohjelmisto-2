f1d5a1912cac800c59b5f0cc31b8ed1a5a24351e
Nichakon Itthikun <nichakon.itthikun@metropolia.fi>
2023-11-14 16:05:34 +0200

    update

---
+++ Matikka/Matikka 2.1.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# Tehtävä 1
+print("1. Tehtävät\n")
+print("1.1 Muunna asteiksi")
+
+a = 2.493
+b = 0.911
+
+ans1 = round(np.degrees(a), 2)
+ans2 = round(np.degrees(b), 2)
+
+print(f"a) 2,493 rad = {ans1} astetta   b) 0,911 rad = {ans2} astetta.")
+# Tehtävä 1
+# 2. muunna asteiksi
+
+c = 137.7
+d = 62.3
+
+ans3 = round(np.radians(c), 2)
+ans4 = round(np.radians(d), 2)
+print("\n1.2 Muunna radiaaneiksi")
+print(f"a) 137,7 astetta = {ans3} rad   b) 62,3 = {ans4} rad.")
